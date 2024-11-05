78ff797388640b72a2335892b82483877dfc6fca
Vitor Navarro <vitorsantosnavarro@gmail.com>
2024-11-05 14:43:13 -0300

    feat: Init Routes Resum Activity

--- app/src/main/java/com/example/diariodebordo/RoutesResumActivity.kt
+++ app/src/main/java/com/example/diariodebordo/RoutesResumActivity.kt
@@ -1,34 +1,20 @@
 package com.example.diariodebordo
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SummaryActivity : AppCompatActivity() {
+class RoutesResumActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_summary)
+        setContentView(R.layout.activity_routes_resum)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val startNewRouteButton: Button = findViewById(R.id.btnStartNewRoute)
-        startNewRouteButton.setOnClickListener{
-            val intent = Intent(this, RouteActivity::class.java)
-            startActivity(intent)
-        }
-
-        val routesResumButton : Button = findViewById(R.id.btnRoutesResum)
-        routesResumButton.setOnClickListener(){
-            val intent = Intent(this, RoutesResumActivity::class.java)
-            startActivity(intent)
         }
     }
 }
