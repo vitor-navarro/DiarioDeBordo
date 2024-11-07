86a4c49da4160b695c5800b6675f8fb38cbccd12
Vitor Navarro <vitorsantosnavarro@gmail.com>
2024-11-07 14:46:04 -0300

    feat: database singleton

--- app/src/main/java/com/example/diariodebordo/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/diariodebordo/data/database/AppDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.diariodebordo.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.diariodebordo.data.database.dao.RouteDao
 import com.example.diariodebordo.data.database.entity.Driver
@@ -10,4 +12,22 @@ import com.example.diariodebordo.data.database.entity.Route
 @Database(entities = [Driver::class, Car::class, Route::class], version = 1)
 abstract class AppDatabase :RoomDatabase(){
   abstract fun routeDao() : RouteDao
+}
+
+object DatabaseSingleton {
+  private var INSTANCE : AppDatabase? = null
+
+  fun getDatabase(context : Context) : AppDatabase{
+    return INSTANCE ?: synchronized(this){
+      val instance = Room.databaseBuilder(
+        context.applicationContext,
+        AppDatabase::class.java,
+        "app_database"
+      ).build()
+      INSTANCE = instance
+      instance
+    }
+  }
+
+
 }
